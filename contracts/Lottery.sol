7f6f52dbefc8db943f679c6618ad84680143276a
abhiram11 <abhiramsatpute@gmail.com>
2023-01-17 22:23:10 +0530

    hardhat compile runs on template sol, now write code

---
+++ contracts/Lottery.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+ //Steps of Lottery App
+//  Participate in the lottery by paying some amount
+// Pick GENUINELY RANDOM winner
+// Winner selected every X minutes = completely automated
+// Chainlink Oracle -> Randomness + Automated Execution using Chainlink Keeper
+
+pragma solidity ^0.8.7;
+
+contract Lottery {}
