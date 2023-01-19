fdcc6580765a24394c8f6457da95acd73d4b281a
abhiram11 <abhiramsatpute@gmail.com>
2023-01-19 15:44:34 +0530

    added event and emit template, now adding logic

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,4 +8,43 @@
 
 pragma solidity ^0.8.7;
 
-contract Lottery {}
+error Lottery__NotEnoughETHEntered();
+
+contract Lottery {
+
+    //state variables = storage
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    //events
+    event participatedEvent(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+
+    function lotteryParticipate() public payable {
+
+        if (msg.value < i_entranceFee) {
+            // revert the whole transaction using error code
+            // so GAS EFFICIENT instead of storing string
+            revert Lottery__NotEnoughETHEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+        // emit and event (both have same name) when we update a dynamic array like s_players
+        emit participatedEvent(msg.sender);
+    }
+
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
+
+    // function randomWinner() {}
+
+}
